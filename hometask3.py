7d2a95cdf50a346b57bd8e94c5938390414cd72f
Mikhail Senchurov <mishkakid96@gmail.com>
2024-10-01 14:12:19 +0300

    done

--- hometask3.py
+++ hometask3.py
@@ -6,16 +6,10 @@ else:
     for i in range(1,n+1):
         print(i**3)
 
+print('\n')
+
 #2
-z = ''
-for x in range(1,11):
-    for y in range(1,10):
-        if x < 10 and x*y < 10:
-            space = '    '
-        elif x < 10 and x*y > 9:
-            space = '   '
-        elif x > 9 and x*y > 9:
-            space = '  '
-        z += f'{y}x{x}={x*y}{space}'
-    z += '\n'
-print(z)
+for i in range(1,10):
+    for j in range(1,10):
+        print('{:2d}'.format(i*j),end=' ')
+    print('')
