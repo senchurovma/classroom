b29614583684d7b0d4e5e1c8e85389ac88e4e3b6
Mikhail Senchurov <mishkakid96@gmail.com>
2024-12-11 01:56:31 +0300

    done

---
+++ qt/main.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import gui
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+
+
+class MainApplication(QtWidgets.QMainWindow, gui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.directory = os.getcwd()
+        self.data = None
+
+
+def main():
+  app = QtWidgets.QApplication(sys.argv)
+  window = MainApplication()
+  window.show()
+  app.exec_()
+
+
+if __name__ == '__main__':
+  main()
